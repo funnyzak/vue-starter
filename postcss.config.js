52d1d0291c0d997cf40f5bdce2cd3954955737ac
leon <silenceace@gmail.com>
2022-07-22 20:31:30 +0800

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-module.exports = (ctx) => ({
+export default (ctx)=> ({
   parser: ctx.parser ? 'sugarss' : false,
   map: ctx.env === 'development' ? ctx.map : false,
   plugins: {
-    'postcss-import': {},
-    'postcss-nested': {},
-    cssnano: ctx.env === 'production' ? {} : false
   }
 })
