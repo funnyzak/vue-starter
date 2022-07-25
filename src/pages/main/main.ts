ac0560233f415d144fbe9a84cceb342a5282b907
leon <silenceace@gmail.com>
2022-07-25 14:23:50 +0800

    main.ts

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue';
+
+// 引入全局样式
 import './style.css';
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// 引入状态管理
+import { setupStore } from '@/store';
+
+// 创建实例
+const setupAll = async () => {
+  const app = createApp(App);
+
+  setupStore(app);
+
+  app.mount('#app');
+};
+
+setupAll();
