b6edd8474ab985f7b08497d05beb0ec44e39bf5f
leon <silenceace@gmail.com>
2022-07-25 13:54:32 +0800

    global type

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,14 @@
 declare interface Fn<T = any> {
-  (...arg: T[]): T
+  (...arg: T[]): T;
 }
 
-declare type Nullable<T> = T | null
+declare type Nullable<T> = T | null;
+
+declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
+
+declare type Recordable<T = any, K = string> = Record<K extends null | undefined ? string : K, T>;
+
+declare type ComponentRef<T> = InstanceType<T>;
 
 declare type LocaleType = 'zh-CN' | 'en'
 
