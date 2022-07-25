2c13354dedd7f1c7b67bc1e1ec529abf8dc9fbc1
leon <silenceace@gmail.com>
2022-07-25 11:35:52 +0800

    feat: app config

---
+++ src/config/app.ts
@@ -0,0 +1,12 @@
+import { useCache } from '@/hooks/web/useCache';
+const { wsCache } = useCache();
+
+export interface AppState {
+  appName: string;
+  isDark: boolean;
+}
+
+export const appModules: AppState = {
+  appName: 'Vite Vue Starter',
+  isDark: wsCache.get('isDark') || false // 是否是暗黑模式
+};
