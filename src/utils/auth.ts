a5350a3d88da62c7836cff6814609cbef3e46353
leon <silenceace@gmail.com>
2022-07-26 19:35:59 +0800

    add auth util tool

---
+++ src/utils/auth.ts
@@ -0,0 +1,70 @@
+import { useCache } from '@/hooks/web/useCache';
+import { TokenType } from '@/api/login/types';
+import { decrypt, encrypt } from '@/utils/jsencrypt';
+
+const { wsCache } = useCache();
+const AccessTokenKey = 'ACCESS_TOKEN';
+const RefreshTokenKey = 'REFRESH_TOKEN';
+
+// 获取token
+export const getAccessToken = () => {
+  return wsCache.get(AccessTokenKey);
+};
+
+// 刷新token
+export const getRefreshToken = () => {
+  return wsCache.get(RefreshTokenKey);
+};
+
+// 设置token
+export const setToken = (token: TokenType) => {
+  wsCache.set(RefreshTokenKey, token.refreshToken, { exp: token.expiresTime });
+  wsCache.set(AccessTokenKey, token.accessToken);
+};
+
+// 删除token
+export const removeToken = () => {
+  wsCache.delete(AccessTokenKey);
+  wsCache.delete(RefreshTokenKey);
+};
+
+const UsernameKey = 'USERNAME';
+const PasswordKey = 'PASSWORD';
+const RememberMeKey = 'REMEMBER_ME';
+
+export const getUsername = () => {
+  return wsCache.get(UsernameKey);
+};
+
+export const setUsername = (username: string) => {
+  wsCache.set(UsernameKey, username);
+};
+
+export const removeUsername = () => {
+  wsCache.delete(UsernameKey);
+};
+
+export const getPassword = () => {
+  const password = wsCache.get(PasswordKey);
+  return password ? decrypt(password) : undefined;
+};
+
+export const setPassword = (password: string) => {
+  wsCache.set(PasswordKey, encrypt(password));
+};
+
+export const removePassword = () => {
+  wsCache.delete(PasswordKey);
+};
+
+export const getRememberMe = () => {
+  return wsCache.get(RememberMeKey) === 'true';
+};
+
+export const setRememberMe = (rememberMe: string) => {
+  wsCache.set(RememberMeKey, rememberMe);
+};
+
+export const removeRememberMe = () => {
+  wsCache.delete(RememberMeKey);
+};
