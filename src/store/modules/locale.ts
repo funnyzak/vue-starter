9501f44b8091f081f0b65249ed0a63169400b803
leon <silenceace@gmail.com>
2022-07-26 11:17:59 +0800

    add locale store

---
+++ src/store/modules/locale.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import { store } from '../index';
+import { useCache } from '@/hooks/web/useCache';
+import { LocaleMap, localeModules } from '@/config/locale';
+import type { LocaleState } from '@/config/locale';
+
+const { wsCache } = useCache();
+
+export const useLocaleStore = defineStore({
+  id: 'locales',
+  state: (): LocaleState => localeModules,
+  persist: {
+    enabled: true
+  },
+  getters: {
+    getCurrentLocale(): string {
+      return this.currentLocale.lang;
+    },
+    getLocaleMap(): LocaleMap {
+      return this.localeMaps.find((item) => item.lang === this.currentLocale.lang) || this.localeMaps[0];
+    }
+  },
+  actions: {
+    setCurrentLocale(localeMap: LocaleMap) {
+      this.currentLocale.lang = localeMap?.lang;
+      wsCache.set('lang', localeMap?.lang);
+    }
+  }
+});
+
+export const useLocaleStoreWithOut = () => {
+  return useLocaleStore(store);
+};
