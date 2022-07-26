d03a7eece6b672796d1ffc569d7d3c179aa3a76d
leon <silenceace@gmail.com>
2022-07-26 13:34:53 +0800

    locales file

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -78,5 +78,15 @@ export default {
   symbol: {
     comma: ',',
     period: '.'
+  },
+  error: {
+    noPermission: `Sorry, you don't have permission to access this page.`,
+    pageError: 'Sorry, the page you visited does not exist.',
+    networkError: 'Sorry, the server reported an error.',
+    returnToHome: 'Return to home'
+  },
+  permission: {
+    hasPermission: `Please set the operation permission label value`,
+    hasRole: `Please set the role permission tag value`
   }
 };
--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -78,5 +78,15 @@ export default {
   symbol: {
     comma: '，',
     period: '。'
+  },
+  error: {
+    noPermission: `抱歉，您无权访问此页面。`,
+    pageError: '抱歉，您访问的页面不存在。',
+    networkError: '抱歉，服务器报告错误。',
+    returnToHome: '返回首页'
+  },
+  permission: {
+    hasPermission: `请设置操作权限标签值`,
+    hasRole: `请设置角色权限标签值`
   }
 };
