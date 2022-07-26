22fff74c34d0251e9548a64cdca9397a9b0c46ef
leon <silenceace@gmail.com>
2022-07-26 13:14:56 +0800

    package check upgrade

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -67,34 +67,34 @@ export const trim = (str: string) => {
  * @param {Date | number | string} time 需要转换的时间
  * @param {String} fmt 需要转换的格式 如 yyyy-MM-dd、yyyy-MM-dd HH:mm:ss
  */
-// export function formatTime(time: Date | number | string, fmt: string) {
-//   if (!time) return ''
-//   else {
-//     const date = new Date(time)
-//     const o = {
-//       'M+': date.getMonth() + 1,
-//       'd+': date.getDate(),
-//       'H+': date.getHours(),
-//       'm+': date.getMinutes(),
-//       's+': date.getSeconds(),
-//       'q+': Math.floor((date.getMonth() + 3) / 3),
-//       S: date.getMilliseconds()
-//     }
-//     if (/(y+)/.test(fmt)) {
-//       fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length))
-//     }
-//     for (const k in o) {
-//       if (new RegExp('(' + k + ')').test(fmt)) {
-//         const _v = o[k as keyof typeof o].toString()
-//         fmt = fmt.replace(
-//           RegExp.$1,
-//           RegExp.$1.length === 1 ? _v : ('00' + _v).substring(('' + _v).length)
-//         )
-//       }
-//     }
-//     return fmt
-//   }
-// }
+export function formatTime(time: Date | number | string, fmt: string) {
+  if (!time) return ''
+  else {
+    const date = new Date(time)
+    const o = {
+      'M+': date.getMonth() + 1,
+      'd+': date.getDate(),
+      'H+': date.getHours(),
+      'm+': date.getMinutes(),
+      's+': date.getSeconds(),
+      'q+': Math.floor((date.getMonth() + 3) / 3),
+      S: date.getMilliseconds()
+    }
+    if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length))
+    }
+    for (const k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+        const _v = o[k as keyof typeof o].toString()
+        fmt = fmt.replace(
+          RegExp.$1,
+          RegExp.$1.length === 1 ? _v : ('00' + _v).substring(('' + _v).length)
+        )
+      }
+    }
+    return fmt
+  }
+}
 
 /**
  * 生成随机字符串
