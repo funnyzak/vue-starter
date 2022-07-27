355940b6370ad773337c91a749546cfee9b3b85c
leon <silenceace@gmail.com>
2022-07-27 10:56:54 +0800

    locales

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -88,5 +88,54 @@ export default {
   permission: {
     hasPermission: `Please set the operation permission label value`,
     hasRole: `Please set the role permission tag value`
+  },
+  sys: {
+    api: {
+      operationFailed: 'Operation failed',
+      errorTip: 'Error Tip',
+      errorMessage: 'The operation failed, the system is abnormal!',
+      timeoutMessage: 'Login timed out, please log in again!',
+      apiTimeoutMessage: 'The interface request timed out, please refresh the page and try again!',
+      apiRequestFailed: 'The interface request failed, please try again later!',
+      networkException: 'network anomaly',
+      networkExceptionMsg:
+        'Please check if your network connection is normal! The network is abnormal',
+
+      errMsg401: 'The user does not have permission (token, user name, password error)!',
+      errMsg403: 'The user is authorized, but access is forbidden!',
+      errMsg404: 'Network request error, the resource was not found!',
+      errMsg405: 'Network request error, request method not allowed!',
+      errMsg408: 'Network request timed out!',
+      errMsg500: 'Server error, please contact the administrator!',
+      errMsg501: 'The network is not implemented!',
+      errMsg502: 'Network Error!',
+      errMsg503: 'The service is unavailable, the server is temporarily overloaded or maintained!',
+      errMsg504: 'Network timeout!',
+      errMsg505: 'The http version does not support the request!',
+      errMsg901: 'Demo mode, no write operations are possible!'
+    },
+    app: {
+      logoutTip: 'Reminder',
+      logoutMessage: 'Confirm to exit the system?',
+      menuLoading: 'Menu loading...'
+    },
+    exception: {
+      backLogin: 'Back Login',
+      backHome: 'Back Home',
+      subTitle403: "Sorry, you don't have access to this page.",
+      subTitle404: 'Sorry, the page you visited does not exist.',
+      subTitle500: 'Sorry, the server is reporting an error.',
+      noDataTitle: 'No data on the current page.',
+      networkErrorTitle: 'Network Error',
+      networkErrorSubTitle:
+        'Sorry, Your network connection has been disconnected, please check your network!'
+    },
+    lock: {
+      unlock: 'Click to unlock',
+      alert: 'Lock screen password error',
+      backToLogin: 'Back to login',
+      entry: 'Enter the system',
+      placeholder: 'Please enter the lock screen password or user password'
+    }
   }
 };
--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -88,5 +88,52 @@ export default {
   permission: {
     hasPermission: `请设置操作权限标签值`,
     hasRole: `请设置角色权限标签值`
+  },
+
+  sys: {
+    api: {
+      operationFailed: '操作失败',
+      errorTip: '错误提示',
+      errorMessage: '操作失败,系统异常!',
+      timeoutMessage: '登录超时,请重新登录!',
+      apiTimeoutMessage: '接口请求超时,请刷新页面重试!',
+      apiRequestFailed: '请求出错，请稍候重试',
+      networkException: '网络异常',
+      networkExceptionMsg: '网络异常，请检查您的网络连接是否正常!',
+      errMsg401: '用户没有权限（令牌、用户名、密码错误）!',
+      errMsg403: '用户得到授权，但是访问是被禁止的。!',
+      errMsg404: '网络请求错误,未找到该资源!',
+      errMsg405: '网络请求错误,请求方法未允许!',
+      errMsg408: '网络请求超时!',
+      errMsg500: '服务器错误,请联系管理员!',
+      errMsg501: '网络未实现!',
+      errMsg502: '网络错误!',
+      errMsg503: '服务不可用，服务器暂时过载或维护!',
+      errMsg504: '网络超时!',
+      errMsg505: 'http版本不支持该请求!',
+      errMsg901: '演示模式，无法进行写操作!'
+    },
+    app: {
+      logoutTip: '温馨提醒',
+      logoutMessage: '是否确认退出系统?',
+      menuLoading: '菜单加载中...'
+    },
+    exception: {
+      backLogin: '返回登录',
+      backHome: '返回首页',
+      subTitle403: '抱歉，您无权访问此页面。',
+      subTitle404: '抱歉，您访问的页面不存在。',
+      subTitle500: '抱歉，服务器报告错误。',
+      noDataTitle: '当前页无数据',
+      networkErrorTitle: '网络错误',
+      networkErrorSubTitle: '抱歉，您的网络连接已断开，请检查您的网络！'
+    },
+    lock: {
+      unlock: '点击解锁',
+      alert: '锁屏密码错误',
+      backToLogin: '返回登录',
+      entry: '进入系统',
+      placeholder: '请输入锁屏密码或者用户密码'
+    }
   }
 };
