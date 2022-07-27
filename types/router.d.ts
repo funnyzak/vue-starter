091f70222d027b8dd449c06f7186a0cde4216754
leon <silenceace@gmail.com>
2022-07-27 17:31:06 +0800

    route type

---
+++ types/router.d.ts
@@ -0,0 +1,51 @@
+import type { RouteRecordRaw } from 'vue-router';
+import { defineComponent } from 'vue';
+
+/**
+* redirect: noredirect        当设置 noredirect 的时候该路由在面包屑导航中不可被点击
+* name:'router-name'          设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
+* meta : {
+
+    title: 'title'            设置该路由在侧边栏和面包屑中展示的名字
+
+    icon: 'svg-name'          设置该路由的图标
+
+    noCache: true             如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+  }
+**/
+declare module 'vue-router' {
+  interface RouteMeta extends Record<string | number | symbol, unknown> {
+    title?: string;
+    icon?: string;
+    noCache?: boolean;
+  }
+}
+
+type Component<T = any> =
+  | ReturnType<typeof defineComponent>
+  | (() => Promise<typeof import('*.vue')>)
+  | (() => Promise<T>);
+
+declare global {
+  declare interface AppRouteRecordRaw extends Omit<RouteRecordRaw, 'meta'> {
+    name: string;
+    meta: RouteMeta;
+    component?: Component | string;
+    children?: AppRouteRecordRaw[];
+    props?: Recordable;
+    fullPath?: string;
+    keepAlive?: boolean;
+  }
+
+  declare interface AppCustomRouteRecordRaw extends Omit<RouteRecordRaw, 'meta'> {
+    icon: any;
+    name: string;
+    meta: RouteMeta;
+    component: string;
+    path: string;
+    redirect: string;
+    children?: AppCustomRouteRecordRaw[];
+    keepAlive?: boolean;
+    visible?: boolean;
+  }
+}
