3b9dd4c624bfe150037a4f2c17d74a0fcac179e7
leon <silenceace@gmail.com>
2022-07-29 09:57:39 +0800

    类型定义

--- types/global.d.ts
+++ types/global.d.ts
@@ -10,27 +10,32 @@ declare type Recordable<T = any, K = string> = Record<K extends null | undefined
 
 declare type ComponentRef<T> = InstanceType<T>;
 
-declare type LocaleType = 'zh-CN' | 'en'
+declare type LocaleType = 'zh-CN' | 'en';
 
 declare type AxiosHeaders =
   | 'application/json'
   | 'application/x-www-form-urlencoded'
-  | 'multipart/form-data'
+  | 'multipart/form-data';
 
-declare type AxiosMethod = 'GET' | 'POST' | 'DELETE' | 'PUT'
+declare type AxiosMethod = 'GET' | 'POST' | 'DELETE' | 'PUT';
 
-declare type AxiosResponseType = 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream'
+declare type AxiosResponseType = 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream';
+
+declare type PermissionLogic = {
+  logic?: 'OR' | 'AND';
+  permissions: string[];
+};
 
 declare interface AxiosConfig {
-  params?: any
-  data?: any
-  url?: string
-  method?: AxiosMethod
-  headersType?: string
-  responseType?: AxiosResponseType
+  params?: any;
+  data?: any;
+  url?: string;
+  method?: AxiosMethod;
+  headersType?: string;
+  responseType?: AxiosResponseType;
 }
 
 declare interface IResponse<T = any> {
-  code?: number
-  data: T extends any ? T : T & any
+  code?: number;
+  data: T extends any ? T : T & any;
 }
