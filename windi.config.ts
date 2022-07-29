17c05712928364fb4c78b7b81ba69345b5f472d6
leon <silenceace@gmail.com>
2022-07-29 13:31:13 +0800

    add windi config

---
+++ windi.config.ts
@@ -0,0 +1,108 @@
+import { defineConfig } from 'windicss/helpers';
+import plugin from 'windicss/plugin';
+import colors from 'windicss/colors';
+
+function range(size, startAt = 1) {
+  return Array.from(Array(size).keys()).map((i) => i + startAt);
+}
+
+export default defineConfig({
+  extract: {
+    include: ['src/**/*.{vue,html,jsx,tsx}'],
+    exclude: ['node_modules', '.git']
+  },
+  darkMode: 'class',
+  attributify: false,
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      },
+      colors: {
+        blue: colors.sky,
+        red: colors.rose,
+        pink: colors.fuchsia
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif']
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      },
+      height: {
+        ...range(50).map((i) => `h-${i}px`)
+      },
+      margin: {
+        ...range(50).map((i) => `mt-${i}px`),
+        ...range(50).map((i) => `mr-${i}px`),
+        ...range(50).map((i) => `mb-${i}px`),
+        ...range(50).map((i) => `ml-${i}px`)
+      }
+    }
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      const newUtilities = {
+        '.skew-10deg': {
+          transform: 'skewY(-10deg)'
+        },
+        '.skew-15deg': {
+          transform: 'skewY(-15deg)'
+        }
+      };
+      addUtilities(newUtilities);
+    }),
+    plugin(({ addComponents }) => {
+      const buttons = {
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600'
+        },
+        '.btn-blue': {
+          'backgroundColor': '#3490dc',
+          'color': '#fff',
+          '&:hover': {
+            backgroundColor: '#2779bd'
+          }
+        },
+        '.btn-red': {
+          'backgroundColor': '#e3342f',
+          'color': '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a'
+          }
+        }
+      };
+      addComponents(buttons);
+    }),
+    plugin(({ addDynamic, variants }) => {
+      addDynamic(
+        'skew',
+        ({ Utility, Style }) => {
+          return Utility.handler
+            .handleStatic(Style('skew'))
+            .handleNumber(0, 360, 'int', (number) => `skewY(-${number}deg)`)
+            .createProperty('transform');
+        },
+        variants('skew')
+      );
+    }),
+    require('windicss/plugin/filters'),
+    require('windicss/plugin/forms'),
+    require('windicss/plugin/aspect-ratio'),
+    require('windicss/plugin/line-clamp'),
+    require('windicss/plugin/typography')({
+      modifiers: ['DEFAULT', 'sm', 'lg', 'red']
+    })
+  ]
+});
