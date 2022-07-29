78e6608a9d89fafb4b2eea70025a3e7229a684a8
leon <silenceace@gmail.com>
2022-07-29 09:58:18 +0800

    utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,38 +5,38 @@
  * @returns any
  */
 export const withInstall = <T>(component: T, alias?: string) => {
-  const comp = component as any
+  const comp = component as any;
   comp.install = (app: any) => {
-    app.component(comp.name || comp.displayName, component)
+    app.component(comp.name || comp.displayName, component);
     if (alias) {
-      app.config.globalProperties[alias] = component
+      app.config.globalProperties[alias] = component;
     }
-  }
-  return component as T & Plugin
-}
+  };
+  return component as T & Plugin;
+};
 
 /**
  * @param str 需要转下划线的驼峰字符串
  * @returns 字符串下划线
  */
 export const humpToUnderline = (str: string): string => {
-  return str.replace(/([A-Z])/g, '-$1').toLowerCase()
-}
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase();
+};
 
 /**
  * @param str 需要转驼峰的下划线字符串
  * @returns 字符串驼峰
  */
 export const underlineToHump = (str: string): string => {
-  if (!str) return ''
+  if (!str) return '';
   return str.replace(/\-(\w)/g, (_, letter: string) => {
-    return letter.toUpperCase()
-  })
-}
+    return letter.toUpperCase();
+  });
+};
 
 export const setCssVar = (prop: string, val: any, dom = document.documentElement) => {
-  dom.style.setProperty(prop, val)
-}
+  dom.style.setProperty(prop, val);
+};
 
 /**
  * 查找数组对象的某个下标
@@ -46,31 +46,31 @@ export const setCssVar = (prop: string, val: any, dom = document.documentElement
 // eslint-disable-next-line
 export const findIndex = <T = Recordable>(ary: Array<T>, fn: Fn): number => {
   if (ary.findIndex) {
-    return ary.findIndex(fn)
+    return ary.findIndex(fn);
   }
-  let index = -1
+  let index = -1;
   ary.some((item: T, i: number, ary: Array<T>) => {
-    const ret: T = fn(item, i, ary)
+    const ret: T = fn(item, i, ary);
     if (ret) {
-      index = i
-      return ret
+      index = i;
+      return ret;
     }
-  })
-  return index
-}
+  });
+  return index;
+};
 
 export const trim = (str: string) => {
-  return str.replace(/(^\s*)|(\s*$)/g, '')
-}
+  return str.replace(/(^\s*)|(\s*$)/g, '');
+};
 
 /**
  * @param {Date | number | string} time 需要转换的时间
  * @param {String} fmt 需要转换的格式 如 yyyy-MM-dd、yyyy-MM-dd HH:mm:ss
  */
 export function formatTime(time: Date | number | string, fmt: string) {
-  if (!time) return ''
+  if (!time) return '';
   else {
-    const date = new Date(time)
+    const date = new Date(time);
     const o = {
       'M+': date.getMonth() + 1,
       'd+': date.getDate(),
@@ -78,21 +78,21 @@ export function formatTime(time: Date | number | string, fmt: string) {
       'm+': date.getMinutes(),
       's+': date.getSeconds(),
       'q+': Math.floor((date.getMonth() + 3) / 3),
-      S: date.getMilliseconds()
-    }
+      'S': date.getMilliseconds()
+    };
     if (/(y+)/.test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length))
+      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length));
     }
     for (const k in o) {
       if (new RegExp('(' + k + ')').test(fmt)) {
-        const _v = o[k as keyof typeof o].toString()
+        const _v = o[k as keyof typeof o].toString();
         fmt = fmt.replace(
           RegExp.$1,
           RegExp.$1.length === 1 ? _v : ('00' + _v).substring(('' + _v).length)
-        )
+        );
       }
     }
-    return fmt
+    return fmt;
   }
 }
 
@@ -101,9 +101,17 @@ export function formatTime(time: Date | number | string, fmt: string) {
  */
 export function toAnyString() {
   const str: string = 'xxxxx-xxxxx-4xxxx-yxxxx-xxxxx'.replace(/[xy]/g, (c: string) => {
-    const r: number = (Math.random() * 16) | 0
-    const v: number = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString()
-  })
-  return str
+    const r: number = (Math.random() * 16) | 0;
+    const v: number = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString();
+  });
+  return str;
+}
+
+/**
+ * 值类型
+ * @param attribute 值
+ */
+export function attrType<T>(attribute: T) {
+  return Object.prototype.toString.call(attribute).slice(8, -1);
 }
---
+++ src/utils/permission.ts
@@ -0,0 +1,40 @@
+import { useCache } from '@/hooks/web/useCache';
+
+/**
+ * 判断用户对某个资源是否有权限
+ * @param permissionValue 要判断的功能权限
+ * @param userPermissions 所拥有的功能权限
+ */
+export default function checkPermission(
+  permissionValue: PermissionLogic,
+  permissionList?: string[]
+) {
+  /**
+   * 不需要权限
+   */
+  if (!permissionValue) {
+    console.log('no need permission');
+    return true;
+  }
+
+  // 如果未传权限列表则使用缓存的权限列表
+  if (!permissionList) {
+    const { wsCache } = useCache();
+    permissionList = (wsCache.get('user').permissions as string[]) || [];
+  }
+
+  const { logic, permissions } = permissionValue;
+
+  if (logic === 'OR' || !logic) {
+    return permissions.some((permission) => {
+      return permissionList!.includes(permission);
+    });
+  } else if (logic === 'AND') {
+    return permissions.every((permission) => {
+      return permissionList!.includes(permission);
+    });
+  } else {
+    console.error(`need permissions! Like v-permission="['user.message']"`);
+    return false;
+  }
+}
